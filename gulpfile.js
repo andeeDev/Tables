c22e5d9b5fee2f9deb9f6c32591241e159e2163f
andee <>
2019-04-23 16:25:40 +0300

    fixed bug with plus minus in accordeon

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const $    = require('gulp-load-plugins')();
 const rename = require('gulp-rename');
 const concat = require('gulp-concat');
-
+const watch = require('gulp-watch');
 var sassPaths = [
     'sass/'
 ];
@@ -16,30 +16,9 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('styles/'))
         .pipe(concat('common.css'))
         .pipe(gulp.dest('styles/'));
-
-            /*.pipe(gulp.dest('styles/')))
-            *  .pipe(rename('common.css'))
-            * */
 });
+gulp.task('stream', function () {
+    // Endless stream mode
+    gulp.watch('sass/**/*.scss', gulp.parallel('sass'));
 
-        // .pipe(gulpfile.dest('D://OpenServer//domains//cabinet.ssu//public//css'))
-        // .pipe($g.sass({
-        //     includePaths: sassPaths,
-        //     outputStyle: 'compressed' // if css compressed **file size**
-        // })
-        //     .on('error', $.sass.logError))
-        // .pipe($.autoprefixer({
-        //     browsers: ['last 2 versions', 'ie >= 9']
-        // }))
-        //.pipe(rename('cabinet.min.css'))
-        // .pipe(gulpfile.dest('css'))
-        // .pipe(gulpfile.dest('D://OpenServer//domains//cabinet.ssu//public//css'));
-
-
-
-//
-// gulpfile.task('default', ['sass'], function() {
-//     gulpfile.watch(['scss/**/*.scss'], ['sass']);
-// });
-
-
+});
